79e5b9086d0052c61caac231e4076d9be999b7df
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2018-04-11 17:39:06 -0400

    Aggregator working

--- StreamingApp/StreamEngine/StreamAggregator.py
+++ StreamingApp/StreamEngine/StreamAggregator.py
@@ -6,130 +6,83 @@ import json
 import argparse
 
 
-def insert(data, stat, response):
-    produce = None
-    if response:
-        if response["timestamp"] > data["curr"]["timestamp"]:
-            produce = data["prev"].copy()
-            if produce["count"]:
-                produce["average"] = produce["summation"] / produce["count"]
+def insert_data(timestamp, cpu, mem, response_time):
+    timestamps = data.keys()
+    try:
+        min_timestamp = min(timestamps)
+    except ValueError:
+        min_timestamp = 0
+    if min_timestamp > timestamp:
+        return timestamps
+    else:
+        if response_time:
+            if timestamp in timestamps:
+                data[timestamp]["summation"] += response_time
+                data[timestamp]["count"] += 1
+                if response_time >= violation:
+                    data[timestamp]["violations"] += 1
+                if data[timestamp]["max"] < response_time:
+                    data[timestamp]["max"] = response_time
             else:
-                produce["average"] = 0
-            data["prev"] = data["curr"].copy()
-            data["curr"] = {}
-            data["curr"]["timestamp"] = response["timestamp"]
-            data["curr"]["summation"] = response["response_time"]
-            data["curr"]["count"] = 1
-            data["curr"]["max"] = response["response_time"]
-            if response["response_time"] >= violation:
-                data["curr"]["violations"] = 1
-            else:
-                data["curr"]["violations"] = 0
-            return produce
-        elif response["timestamp"] == data["curr"]["timestamp"]:
-            data["curr"]["summation"] += response["response_time"]
-            data["curr"]["count"] += 1
-            if response["response_time"] >= violation:
-                data["curr"]["violations"] += 1
-            if data["curr"]["max"] < response["response_time"]:
-                data["curr"]["max"] = response["response_time"]
-            return produce
-        elif response["timestamp"] == data["prev"]["timestamp"]:
-            data["prev"]["summation"] += response["response_time"]
-            data["prev"]["count"] += 1
-            if response["response_time"] >= violation:
-                data["prev"]["violations"] += 1
-            if data["prev"]["max"] < response["response_time"]:
-                data["prev"]["max"] = response["response_time"]
+                timestamps.append(timestamp)
+                data[timestamp] = {}
+                data[timestamp]["summation"] = response_time
+                data[timestamp]["count"] = 1
+                if response_time >= violation:
+                    data[timestamp]["violations"] = 1
+                else:
+                    data[timestamp]["violations"] = 0
+                data[timestamp]["max"] = response_time
         else:
-            return produce
-
-    elif stat:
-        if stat["timestamp"] > data["curr"]["timestamp"]:
-            produce = data["prev"].copy()
-            if produce["count"]:
-                produce["average"] = produce["summation"] / produce["count"]
-            else:
-                produce["average"] = 0
-            data["prev"] = data["curr"].copy()
-            data["curr"] = {}
-            data["curr"]["timestamp"] = stat["timestamp"]
-            data["curr"]["max"] = 0
-            data["curr"]["cpu"] = stat["cpu"]
-            data["curr"]["mem"] = stat["mem"]
-            data["curr"]["count"] = 0
-            data["curr"]["violations"] = 0
-            return produce
-        elif stat["timestamp"] == data["curr"]["timestamp"]:
-            data["curr"]["cpu"] = stat["cpu"]
-            data["curr"]["mem"] = stat["mem"]
-            return produce
-        elif stat["timestamp"] == data["prev"]["timestamp"]:
-            data["prev"]["cpu"] = stat["cpu"]
-            data["prev"]["mem"] = stat["mem"]
-        else:
-            return produce
+            if timestamp not in timestamps:
+                data[timestamp] = {}
+                timestamps.append(timestamp)
+            data[timestamp]["cpu"] = cpu
+            data[timestamp]["mem"] = mem
+    return timestamps
 
 
 def response_time_aggregator():
     response_time_topic = 'responsetime'
     stats_topic = 'stats'
-    aggregate_topic = 'aggregate2'
+    aggregate_topic = 'aggregate'
 
     while True:
         try:
-            data = {
-                "curr": {
-                    "timestamp": 0,
-                    "cpu": 0,
-                    "mem": 0,
-                    "count": 0,
-                    "summation": 0,
-                    "average": 0,
-                    "max": 0,
-                    "violations": 0
-                },
-                "prev": {
-                    "timestamp": 0,
-                    "cpu": 0,
-                    "mem": 0,
-                    "count": 0,
-                    "summation": 0,
-                    "average": 0,
-                    "max": 0,
-                    "violations": 0
-                }
-            }
-
             aggregate_producer = KafkaProducer(bootstrap_servers=[bootstrap_servers])
             response_time_consumer = KafkaConsumer(response_time_topic,
-                                                   auto_offset_reset='earliest',
-                                                   # auto_offset_reset='latest',
+                                                   # auto_offset_reset='earliest',
+                                                   auto_offset_reset='latest',
                                                    bootstrap_servers=[bootstrap_servers])
             stats_consumer = KafkaConsumer(stats_topic,
-                                           auto_offset_reset='earliest',
-                                           # auto_offset_reset='latest',
+                                           # auto_offset_reset='earliest',
+                                           auto_offset_reset='latest',
                                            bootstrap_servers=[bootstrap_servers])
 
             while True:
                 # Consume stats every second
                 msg = stats_consumer.next()
                 message = msg.value.split(',')
-                produce = insert(data=data,
-                                 stat={"timestamp": int(message[0]), "cpu": int(message[1]), "mem": int(message[2])},
-                                 response=None)
-                if produce:
-                    aggregate_producer.send(aggregate_topic, json.dumps(produce))  # not a synchronous send
+                timestamps = insert_data(timestamp=int(message[0]),
+                                         cpu=int(message[1]),
+                                         mem=int(message[2]),
+                                         response_time=None)
 
-                # Consumer responses for the entire second
-                produce = None
-                while not produce:
+                # Consume responses for until third moment arrives
+                while len(timestamps) < 3:
                     msg = response_time_consumer.next()
                     message = msg.value.split(',')
-                    produce = insert(data=data,
-                                     response={"timestamp": int(message[1]), "response_time": int(message[0])},
-                                     stat=None)
-                aggregate_producer.send(aggregate_topic, json.dumps(produce))  # not a synchronous send
+                    timestamps = insert_data(timestamp=int(message[1]),
+                                             response_time=int(message[0]),
+                                             cpu=None,
+                                             mem=None)
+                if len(timestamps) > 2:
+                    timestamp = min(timestamps)
+                    aggregate = data.pop(timestamp)
+                    aggregate["timestamp"] = timestamp
+                    print aggregate
+                    if len(aggregate.keys()) == 7:  # Produce clean data
+                        aggregate_producer.send(aggregate_topic, json.dumps(aggregate))  # not a synchronous send
 
         except kafka.errors.NoBrokersAvailable:
             pass
@@ -141,5 +94,6 @@ parser.add_argument("-p", "--port", help="Port number of Kafka", default='9092',
 parser.add_argument("-v", "--violation", help="SLO violation limit (milliseconds)", default=100, type=int, required=False)
 args = parser.parse_args()
 violation = args.violation
+data = {}
 bootstrap_servers = ":".join([args.ip, args.port])
 response_time_aggregator()

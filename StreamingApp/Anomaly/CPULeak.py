d59408955b491162823b644da8d0f5ae3629d064
Pratik <pratik.iem01@gmail.com>
2018-04-13 12:17:49 -0400

    Update CPULeak.py

--- StreamingApp/Anomaly/CPULeak.py
+++ StreamingApp/Anomaly/CPULeak.py
@@ -5,18 +5,18 @@ def cpuLeak():
 	# 1s, it first uses the CPU for 1%, 2%,... 100%
 	percent = 1
 	while True:
-		sms = (int)time.time()
+		sms = (int)(time.time())
 		ems = sms + 1000
 		x=2
 		percent = percent + 1
 		emsCpu = sms + 10*percent
 		
 		# compute for percent fraction of CPU
-		while (int)time.time() < emsCpu:
+		while ((int)time.time()) < emsCpu:
 			x=x*2
 			
 		#sleep for the remaining time 
-		time.sleep((ems - (int)time.time())/1000)
+		time.sleep((ems - ((int)time.time()))/1000)
 
 	
 if __name__ == "__main__":

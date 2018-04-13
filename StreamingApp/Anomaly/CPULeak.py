8fc56a272e2eff3d1e5fd20ebb354b0ca01d39e5
Pratik <pratik.iem01@gmail.com>
2018-04-13 13:01:45 -0400

    CPU leak, 1s->1%CPU, 2s->2%CPU, ... 100s -> 100% CPU

--- StreamingApp/Anomaly/CPULeak.py
+++ StreamingApp/Anomaly/CPULeak.py
@@ -1,23 +1,32 @@
 import time
 
 def cpuLeak():
-	# takes up cpu cycles incrementally
-	# 1s, it first uses the CPU for 1%, 2%,... 100%
-	percent = 1
-	while True:
-		sms = (int)(time.time())
-		ems = sms + 1000
-		x=2
-		percent = percent + 1
-		emsCpu = sms + 10*percent
-		
-		# compute for percent fraction of CPU
-		while ((int)time.time()) < emsCpu:
-			x=x*2
-			
-		#sleep for the remaining time 
-		time.sleep((ems - ((int)time.time()))/1000)
+        # takes up cpu cycles incrementally
+        # 1s, it first uses the CPU for 1%, 2%,... 100%
+        percent = 1
+        while True:
+                ss = (int)(time.time())*1000
+                es = ss + 1000
+                x=2
+                percent = percent + 1
+                esCpu = ss + (float)(percent*10)
 
-	
+                print("compute Step"+str(percent))
+                print(ss)
+                print(esCpu)
+
+                # compute for percent fraction of CPU
+                while (time.time()*1000) < esCpu:
+                        x=x*2
+
+                #sleep for the remaining time
+                sltime = (float)(es - (int)(time.time()*1000))/1000
+                print(sltime)
+                time.sleep(sltime)
+
+                if(percent==100):
+                        break
+
+
 if __name__ == "__main__":
-    cpuLeak()
+        cpuLeak()

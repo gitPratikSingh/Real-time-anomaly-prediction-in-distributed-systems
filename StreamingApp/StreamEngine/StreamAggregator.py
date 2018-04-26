a7a4dd205706d62ac9e636bb7f68d9f987e70cd2
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2018-04-25 21:40:13 -0400

    aggregation detecting anomaly

--- StreamingApp/StreamEngine/StreamAggregator.py
+++ StreamingApp/StreamEngine/StreamAggregator.py
@@ -26,6 +26,7 @@ def insert_data(timestamp, cpu, mem, response_time):
                 except KeyError:
                     data[timestamp]["count"] = 1
                 if response_time >= violation:
+                    print "*** VIOLATION: {} ***".format(response_time)
                     try:
                         data[timestamp]["violations"] += 1
                     except KeyError:
@@ -49,22 +50,21 @@ def insert_data(timestamp, cpu, mem, response_time):
             if timestamp not in timestamps:
                 data[timestamp] = {}
                 timestamps.append(timestamp)
+                data[timestamp]["violations"] = 0
             data[timestamp]["cpu"] = cpu
             data[timestamp]["mem"] = mem
-            if "violations" not in data.keys():
-                data[timestamp]["violations"] = 0
+
     return timestamps
 
 
 def response_time_aggregator():
     response_time_topic = 'responsetime'
-    reset = 3
     stats_topic = 'stats'
     aggregate_topic = 'aggregate'
     delay = 6
     with open("aggregate", "a+") as f:
-        while True:
-            try:
+        try:
+            while True:
                 aggregate_producer = KafkaProducer(bootstrap_servers=[bootstrap_servers])
                 response_time_consumer = KafkaConsumer(response_time_topic,
                                                        # auto_offset_reset='earliest',
@@ -84,7 +84,7 @@ def response_time_aggregator():
                                              mem=int(message[2]),
                                              response_time=None)
 
-                    # Consume responses for until third moment arrives
+                    # Consume responses for until the delay
                     while len(timestamps) < delay:
                         msg = response_time_consumer.next()
                         message = msg.value.split(',')
@@ -93,10 +93,12 @@ def response_time_aggregator():
                                                  cpu=None,
                                                  mem=None)
                     if len(timestamps) > delay - 1:
+
                         timestamp = min(timestamps)
                         aggregate = data.pop(timestamp)
                         aggregate["timestamp"] = timestamp
-
+                        if not aggregate['cpu']:
+                            raise KeyError
                         if len(aggregate.keys()) == 7:  # Produce clean data
                             try:
                                 aggregate["mean"] = aggregate['summation'] / aggregate['count']
@@ -107,16 +109,16 @@ def response_time_aggregator():
                             f.writelines(jsondump + "\n")
                             print(jsondump + "\n")
 
-            except kafka.errors.NoBrokersAvailable:
-                pass
-
-
+        except kafka.errors.NoBrokersAvailable:
+            pass
+        except KeyError:
+            pass
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--ip", help="IP/Hostname of Kafka instance", default='172.25.130.9', required=False)
 parser.add_argument("-p", "--port", help="Port number of Kafka", default='9092', required=False)
-parser.add_argument("-v", "--violation", help="SLO violation limit (milliseconds)", default=100, type=int, required=False)
+parser.add_argument("-v", "--violation", help="SLO violation limit (milliseconds)", default=70, type=int, required=False)
 args = parser.parse_args()
 violation = args.violation
 data = {}

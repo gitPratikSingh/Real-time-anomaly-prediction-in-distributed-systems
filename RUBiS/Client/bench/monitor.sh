64ed5ce07444d0464a5fb921164f3b8d7bb5c6fe
Anirudha Tambolkar <anirudhatambolkar@gmail.com>
2018-05-05 20:21:40 -0400

    Removed code from RUBIS repo

--- RUBiS/Client/bench/monitor.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/usr/bin/ssh $1 /usr/bin/sar -n DEV -n SOCK -rubwFB $2 $3 > $4
--- RUBiS/database/update_old_items.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO old_items SELECT * FROM items WHERE end_date < "2001-10-18 16:17:00";
-DELETE FROM items WHERE end_date < "2001-10-18 16:17:00";
